48c5f222063a88ef96021f8119f16c049101ebb9
Daniel Berio <drand48@gmail.com>
2016-06-19 15:18:37 +0100

    console

--- console.h
+++ console.h
@@ -10,6 +10,9 @@ struct Console
     int                   historyPos;    // -1: new line, 0..history.Size-1 browsing history.
     ImVector<const char*> commands;
     
+    float inputHeight = 30;
+    bool consoleOpen = false;
+    
     Console()
     {
         clear();
@@ -54,7 +57,120 @@ struct Console
         scrollToBottom = true;
     }
     
-    void    draw(const char* title, bool* p_open)
+    void    draw(float width, float height)
+    {
+        float H = ImGui::GetIO().DisplaySize.y;
+        
+        ImVec2 size = ImVec2(width,height);
+        ImGui::SetNextWindowSize(size);
+        if(consoleOpen)
+        {
+            ImGui::SetNextWindowPos(ImVec2(0,H-inputHeight-height));
+        }
+        else
+        {
+            ImGui::SetNextWindowPos(ImVec2(0,H-inputHeight));
+        }
+        
+        static ImGuiTextFilter filter;
+        
+        if(consoleOpen)
+        {
+            ImGui::Begin("console",&consoleOpen, size, 0.5f, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove|ImGuiWindowFlags_NoSavedSettings );
+            
+            //ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoTitleBar|
+            //ImGuiWindowFlags_NoScrollbar
+            
+            
+            /*
+            ImGui::BeginChild("ScrollingRegion", ImVec2(0,-ImGui::GetItemsLineHeightWithSpacing()), false, ImGuiWindowFlags_HorizontalScrollbar);
+            */
+            // Display every line as a separate entry so we can change their color or add custom widgets. If you only want raw text you can use ImGui::TextUnformatted(log.begin(), log.end());
+            // NB- if you have thousands of entries this approach may be too inefficient and may require user-side clipping to only process visible items.
+            // You can seek and display only the lines that are visible using the ImGuiListClipper helper, if your elements are evenly spaced and you have cheap random access to the elements.
+            // To use the clipper we could replace the 'for (int i = 0; i < items.Size; i++)' loop with:
+            //     ImGuiListClipper clipper(items.Size);
+            //     while (clipper.Step())
+            //         for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
+            // However take note that you can not use this code as is if a filter is active because it breaks the 'cheap random-access' property. We would need random-access on the post-filtered list.
+            // A typical application wanting coarse clipping and filtering may want to pre-compute an array of indices that passed the filtering test, recomputing this array when user changes the filter,
+            // and appending newly elements as they are inserted. This is left as a task to the user until we can manage to improve this example code!
+            // If your items are of variable size you may want to implement code similar to what ImGuiListClipper does. Or split your data into fixed height items to allow random-seeking into your list.
+            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4,1)); // Tighten spacing
+            for (int i = 0; i < items.Size; i++)
+            {
+                const char* item = items[i];
+                if (!filter.PassFilter(item))
+                    continue;
+                ImVec4 col = ImVec4(1.0f,1.0f,1.0f,1.0f); // A better implementation may store a type per-item. For the sample let's just parse the text.
+                if (strstr(item, "[error]") || strstr(item, "Traceback")) col = ImColor(1.0f,0.4f,0.4f,1.0f);
+                else if (strncmp(item, "# ", 2) == 0) col = ImColor(1.0f,0.78f,0.58f,1.0f);
+                ImGui::PushStyleColor(ImGuiCol_Text, col);
+                ImGui::TextUnformatted(item);
+                ImGui::PopStyleColor();
+            }
+            if (scrollToBottom)
+                ImGui::SetScrollHere();
+            scrollToBottom = false;
+            ImGui::PopStyleVar();
+            //ImGui::EndChild();
+            ImGui::End();
+        }
+        
+        // Input line
+        size = ImVec2(width,inputHeight);
+        ImGui::SetNextWindowPos(ImVec2(0,H-inputHeight));
+        ImGui::SetNextWindowSize(size);
+        static bool p_open = true;
+        ImGui::Begin("console input",&p_open, size, 1.0f, ImGuiWindowFlags_NoTitleBar|ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoMove|ImGuiWindowFlags_NoSavedSettings);
+        
+        if(ImGui::IconSelectable("e",consoleOpen))
+            consoleOpen = !consoleOpen;
+        ImGui::SameLine();
+        
+        ImGui::Text(">>");
+        ImGui::SameLine();
+        ImGui::PushItemWidth(width-400);
+        
+        // Command-line
+        if (ImGui::InputText(" ", inputBuf, ARRAYSIZE(inputBuf), ImGuiInputTextFlags_EnterReturnsTrue|ImGuiInputTextFlags_CallbackCompletion|ImGuiInputTextFlags_CallbackHistory, &textEditCallbackStub, (void*)this))
+        {
+            char* input_end = inputBuf+strlen(inputBuf);
+            while (input_end > inputBuf && input_end[-1] == ' ') input_end--; *input_end = 0;
+            if (inputBuf[0])
+                execCommand(inputBuf);
+            strcpy(inputBuf, "");
+        }
+        
+        // Demonstrate keeping auto focus on the input box
+        if (ImGui::IsItemHovered() || (ImGui::IsRootWindowOrAnyChildFocused() && !ImGui::IsAnyItemActive() && !ImGui::IsMouseClicked(0)))
+            ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
+        
+        ImGui::PopItemWidth();
+        
+        ImGui::SameLine();
+        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0,0));
+        filter.Draw("Filter", 100);
+        ImGui::PopStyleVar();
+        
+        //        ImGui::TextWrapped("This example implements a console with basic coloring, completion and history. A more elaborate implementation may want to store entries along with extra data such as timestamp, emitter, etc.");
+        //        ImGui::TextWrapped("Enter 'HELP' for help, press TAB to use text completion.");
+        
+        // TODO: display items starting from the bottom
+        ImGui::SameLine();
+        if (ImGui::SmallButton("Clear")) clear();
+        ImGui::SameLine();
+        if (ImGui::SmallButton("Bottom")) scrollToBottom = true;
+        //static float t = 0.0f; if (ImGui::GetTime() - t > 0.02f) { t = ImGui::GetTime(); log("Spam %f", t); }
+        
+        
+        
+        
+        ImGui::End();
+    }
+
+    
+    void    draw_old(const char* title, bool* p_open)
     {
         ImGui::SetNextWindowSize(ImVec2(520,600), ImGuiSetCond_FirstUseEver);
         if (!ImGui::Begin(title, p_open))
@@ -138,7 +254,7 @@ struct Console
     void    execCommand(const char* command_line)
     {
         log("# %s\n", command_line);
-        
+        consoleOpen = true;
         // Insert into history. First find match and delete it so it can be pushed to the back. This isn't trying to be smart or optimal.
         historyPos = -1;
         for (int i = history.Size-1; i >= 0; i--)
