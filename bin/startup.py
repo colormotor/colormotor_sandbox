fc3478c7b547d85735f667d91b5114c4d245e30b
Daniel Berio <drand48@gmail.com>
2018-01-16 15:56:06 -0800

    startup fix - needs cleanup

--- bin/startup.py
+++ bin/startup.py
@@ -1,7 +1,17 @@
 print "Executing startup script"
 import math
 import numpy as np
+import scipy as sp
 import os
 import sys
+
+# NetworkX seems to be necessary for reload issues
+print("loading networkx and matplotlib")
+import networkx as nx
+import networkx
+#import matplotlib
+print("loading tensorflow")
 import tensorflow as tf
-print "Success."
+
+
+print("Success.")
